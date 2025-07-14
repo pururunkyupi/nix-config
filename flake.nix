efd5d790f8baa58248a22e774efb0f04a7745b0d
pururunkyupi <pururunkyupi@tutamail.com>
2025-07-14 03:39:44 +0000

    update flake github repos

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
